8bb78a4ce2e60f765187e5ea9e2ac639c213e47d
Jillian Meehan <jillian@lickability.net>
2020-03-24 20:45:21 -0400

    link stuff!!!!

--- anime.js
+++ anime.js
@@ -1,33 +1,35 @@
+// Find all links in the body of a paragraph
+var link = document.querySelectorAll("p a");
 
-
-// Wrap each letter in a span
-var textWrapper = document.querySelectorAll("p a");
-textWrapper.forEach(
+link.forEach(
   function(item) {
+    // Wrap each letter in a span
     item.innerHTML = item.textContent.replace(
       /\S/g,
       "<span class='letter'>$&</span>"
     );
+
+    // Play animation on hover
+    item.onmouseenter = function() {
+      animation.play();
+    };
+
+    // Stop animation
+    item.onmouseleave = function() {
+      animation.pause();
+      animation.seek(0);
+    };
+
+    // Animatation
+    var animation = anime({
+      targets: item.querySelectorAll(".letter"),
+      translateY: [0, "-0.16em", 0],
+      translateZ: 0,
+      duration: 1000,
+      easing: 'easeInOutBack',
+      delay: anime.stagger(50),
+      loop: true,
+      autoplay: false
+    });
   },
 );
-
-// Animatation
-var animation = anime({
-  targets: "a .letter",
-  translateY: [0, "-0.16em", 0],
-  translateZ: 0,
-  duration: 500,
-  easing: 'linear',
-  delay: anime.stagger(100),
-  loop: true,
-  autoplay: false
-});
-
-textWrapper.onmouseenter = function() {
-  animation.play();
-};
-
-textWrapper.onmouseleave = function() {
-  animation.pause();
-  animation.seek(0);
-};
